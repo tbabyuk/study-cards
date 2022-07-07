6eca04667b6d3c46364c56fba6779b2bca5c8d0f
Terry Babyuk <tbabyuk@gmail.com>
2022-07-07 00:00:28 -0400

    create card modal functionality working

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 
+const cardsWrapper = document.getElementById("cards-wrapper");
+const prevBtn = document.getElementById("btn-prev");
+const nextBtn = document.getElementById("btn-next");
+const currentCard = document.getElementById("current-card");
+const openFormBtn = document.getElementById("btn-open-form");
+const closeFormBtn = document.getElementById("btn-close-form");
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+const createCardBtn = document.getElementById("btn-create-card");
+const newCardModal = document.getElementById("create-card-overlay");
+
+
+createCardBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const q = question.value;
+    const a = answer.value;
+    console.log(q, a)
+})
+
+
+openFormBtn.addEventListener("click", () => {
+    newCardModal.className= " ";
+})
+
+closeFormBtn.addEventListener("click", () => {
+    newCardModal.className= "hidden";
+})
+
+
+
 const actionBtn = document.querySelector('.action');
 const card3 = document.querySelector(".card3");
 const flipper = document.querySelector(".flipper");
