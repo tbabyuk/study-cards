e7e3e8c3e9241cc0dd5281fe6cfb916e1575f4f7
Terry Babyuk <tbabyuk@gmail.com>
2022-07-08 01:04:14 -0400

    next and prev card functionality working!

--- script.js
+++ script.js
@@ -1,41 +1,160 @@
 
+const addCardModal = document.querySelector(".add-card-modal");
 const cardsWrapper = document.getElementById("cards-wrapper");
-const prevBtn = document.getElementById("btn-prev");
-const nextBtn = document.getElementById("btn-next");
+const cards = document.querySelectorAll(".card");
+const btnAddCard = document.getElementById("btn-add-card");
+const btnCloseForm = document.getElementById("btn-close-form");
+const btnFormSubmit = document.getElementById("btn-form-submit");
+const formQuestion = document.getElementById("question");
+const formAnswer = document.getElementById("answer");
 const currentCard = document.getElementById("current-card");
-const openFormBtn = document.getElementById("btn-open-form");
-const closeFormBtn = document.getElementById("btn-close-form");
-const question = document.getElementById("question");
-const answer = document.getElementById("answer");
-const createCardBtn = document.getElementById("btn-create-card");
-const newCardModal = document.getElementById("create-card-overlay");
+const btnNextCard = document.getElementById("btn-next-card");
+const btnPrevCard = document.getElementById("btn-prev-card");
 
 
-createCardBtn.addEventListener("click", (e) => {
+//Current active card
+let currentActiveCard = 0;
+
+
+//Store DOM cards
+const cardsEl = [];
+
+
+//Card data
+const cardsData = [
+    {
+        question: "Card 1 question?",
+        answer: "Card 1 answer"
+    },
+    {
+        question: "Card 2 question?",
+        answer: "Card 2 answer"
+    },
+    {
+        question: "Card 3 question?",
+        answer: "Card 3 answer"
+    }
+];
+
+
+//Create cards based on card data
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+};
+
+//Create a single card
+function createCard(data, index) {
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    if(index === 0) {
+        card.classList.add("active")
+    }
+
+    card.innerHTML = `
+    <div class="card-inner">
+    <div class="card-front">${data.question}</div>
+    <div class="card-back">${data.answer}</div>
+    </div>
+    <span class="btn-flip">FLIP</span>
+    `;
+
+    card.addEventListener("click", () => {
+        card.classList.toggle("flip")
+    })
+
+    //Add to DOM cards
+    cardsEl.push(card);
+    cardsWrapper.appendChild(card);
+}
+
+createCards();
+
+
+//EVENT LISTENERS
+
+btnNextCard.addEventListener("click", () => {
+    // cardsEl[currentActiveCard].className = 
+    console.log(currentActiveCard)
+    console.log(cardsEl)
+
+    cardsEl[currentActiveCard].className = 'card';
+    currentActiveCard = currentActiveCard + 1;
+
+    if(currentActiveCard > cardsEl.length -1) {
+        currentActiveCard = cardsEl.length - 1;
+    }
+
+    cardsEl[currentActiveCard].className = 'card active';
+
+    updateCurrentCards();
+});
+
+btnPrevCard.addEventListener("click", () => {
+    // cardsEl[currentActiveCard].className = 
+    console.log(currentActiveCard)
+    cardsEl[currentActiveCard].className = 'card';
+
+    currentActiveCard = currentActiveCard - 1;
+
+    if(currentActiveCard < 0) {
+        currentActiveCard = 0;
+    }
+
+    cardsEl[currentActiveCard].className = 'card active';
+
+    updateCurrentCards();
+});
+
+
+
+btnAddCard.addEventListener("click", () => {
+    addCardModal.classList.remove("hidden");
+    cardsWrapper.style.display = "none"
+})
+
+addCardModal.addEventListener("click", (e) => {
+    if(e.target.className === "add-card-modal") {
+        addCardModal.classList.add("hidden");
+        cardsWrapper.style.display = "block"
+
+    }
+})
+
+btnCloseForm.addEventListener("click", () => {
+    addCardModal.classList.add("hidden");
+    cardsWrapper.style.display = "block"
+
+})
+
+
+updateCurrentCards();
+
+function updateCurrentCards() {
+    currentCard.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
+
+
+btnFormSubmit.addEventListener("click", (e) => {
     e.preventDefault();
-    const q = question.value;
-    const a = answer.value;
-    console.log(q, a)
+    const newCard = {
+        question: `${formQuestion.value}`,
+        answer: `${formAnswer.value}`
+    }
+
+    cardsData.push(newCard);
+    console.log(cardsData);
+    addCardModal.classList.add("hidden");
+    updateCurrentCards();
 })
 
 
-openFormBtn.addEventListener("click", () => {
-    newCardModal.className= " ";
-})
 
-closeFormBtn.addEventListener("click", () => {
-    newCardModal.className= "hidden";
-})
-
-
-
-const actionBtn = document.querySelector('.action');
-const card3 = document.querySelector(".card3");
-const flipper = document.querySelector(".flipper");
-const innerCard = document.querySelector(".card-inner");
-
-
-
-flipper.addEventListener('click', () => {
-    innerCard.classList.toggle("rotate");
-})
+// const btnFlip = document.querySelectorAll(".btn-flip");
+// btnFlip.forEach(btn => {
+//     btn.addEventListener("click", (e) => {
+//         const currCard = e.target.parentElement;
+//         currCard.classList.toggle("rotate")
+//     })
+// });
